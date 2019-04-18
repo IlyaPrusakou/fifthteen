2af206b18a22df25a155178c810b14c788b6fa5c
Ilya Prusakou <beyourself@tut.by>
2019-04-19 02:48:31 +0400

    pre relise fifithteen

---
+++ fifthteen/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifthteen
+{
+    class Class1
+    {
+        public int size;
+        public int zeroX;
+        public int zeroY;
+        public int[,] mass;
+        public Random rnd = new Random();
+        
+        public int currentX;
+        public int currentY;
+
+        public Class1(int size)
+        {
+            if (size < 2)
+            {
+                size = 2;
+            }
+            if (size > 5)
+            {
+                size = 5;
+            }
+            this.size = size;
+            mass = new int[size, size];
+        }
+
+        public void Start()
+        {
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    mass[y, x] = CoordsToPosition(x, y) + 1; 
+                }
+            }
+            
+            zeroX = rnd.Next(0,3);
+            zeroY = rnd.Next(0, 3);
+            mass[zeroX,zeroY] = 0;
+            mass[3, 3] = CoordsToPosition(zeroY, zeroX) + 1;
+        }
+
+        
+
+        public int CoordsToPosition(int x, int y)
+        {
+            return y * size + x;
+        }
+        public void PositionToCoords(int position, out int x, out int y)
+        {
+            x = position % size;
+            y = position / size;
+        }
+        
+        
+        public  int FindOutCurrentYPosition()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (mass[i, j] == 0)
+                    {
+                        
+                        currentY = i;
+                        break;
+
+                    }
+                }
+            }
+            return currentY;
+        }
+        public  int FindOutCurrentXPosition()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (mass[i, j] == 0)
+                    {
+                        
+                        currentX = j;
+                        break;
+
+                    }
+                }
+            }
+            return currentY;
+        }
+
+        
+    }
+}
--- fifthteen/Program.cs
+++ fifthteen/Program.cs
@@ -8,8 +8,124 @@ namespace fifthteen
 {
     class Program
     {
+        public static Class1 game = new Class1(4);
+  
+        public static void SwapUp()
+        {
+            int v;
+            game.FindOutCurrentXPosition();
+            game.FindOutCurrentYPosition();
+            if (game.currentY > 0)
+            {
+                v = game.mass[game.currentY - 1, game.currentX];
+                game.mass[game.currentY, game.currentX] = v;
+                game.mass[game.currentY - 1, game.currentX] = 0;
+            }
+            
+        }
+        public static void SwapDown()
+        {
+            int v;
+            game.FindOutCurrentXPosition();
+            game.FindOutCurrentYPosition();
+            if (game.currentY < 3)
+            {
+                v = game.mass[game.currentY + 1, game.currentX];
+                game.mass[game.currentY, game.currentX] = v;
+                game.mass[game.currentY + 1, game.currentX] = 0;
+            }
+            
+        }
+        public static void SwapLeft()
+        {
+            int v;
+            game.FindOutCurrentXPosition();
+            game.FindOutCurrentYPosition();
+            if (game.currentX > 0)
+            {
+                v = game.mass[game.currentY, game.currentX-1];
+                game.mass[game.currentY, game.currentX] = v;
+                game.mass[game.currentY, game.currentX-1] = 0;
+            }
+
+        }
+        public static void SwapRight()
+        {
+            int v;
+            game.FindOutCurrentXPosition();
+            game.FindOutCurrentYPosition();
+            if (game.currentX < 3)
+            {
+                v = game.mass[game.currentY, game.currentX + 1];
+                game.mass[game.currentY, game.currentX] = v;
+                game.mass[game.currentY, game.currentX + 1] = 0;
+            }
+
+        }
+        public static void Render()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+
+                    Console.Write($"{game.mass[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+
         static void Main(string[] args)
         {
+            game.Start();
+            Console.WriteLine(game.mass[0, 0]);
+            Console.WriteLine(game.mass[0, 1]);
+            Console.WriteLine(game.mass[0, 2]);
+            Console.WriteLine(game.mass[0, 3]);
+            Console.WriteLine(game.mass[1, 0]);
+            Console.WriteLine(game.mass[1, 1]);
+            Console.WriteLine(game.mass[1, 2]);
+            Console.WriteLine(game.mass[1, 3]);
+            Console.WriteLine(game.mass[2, 0]);
+            Console.WriteLine(game.mass[2, 1]);
+            Console.WriteLine(game.mass[2, 2]);
+            Console.WriteLine(game.mass[2, 3]);
+            Console.WriteLine(game.mass[3, 0]);
+            Console.WriteLine(game.mass[3, 1]);
+            Console.WriteLine(game.mass[3, 2]);
+            Console.WriteLine(game.mass[3, 3]);
+            Render();
+            Console.WriteLine("Input wasd");
+            while (true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "w":
+                        SwapUp();
+                        Render();
+                        break;
+                        case "s":
+                        SwapDown();
+                        Render();
+                        break;
+                        case "a":
+                        SwapLeft();
+                        Render();
+                        break;
+                        case "d":
+                        SwapRight();
+                        Render();
+                        break;
+                }
+            }
+
+            
+            
+
+            
+            Console.ReadKey();
         }
     }
 }
